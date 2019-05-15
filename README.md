85ffce5337afb9fecb82324029ed005d62b3146c
kevin <kevin81319@gmail.com>
2019-05-15 09:32:15 +0800

    combined to one script

--- README.md
+++ README.md
@@ -1,54 +1,57 @@
 # 儀表版資料前處理腳本說明
 
 - ### 腳本使用說明:
-	1. 下載腳本至本機後，請使用R-Studio打開檔案並先安裝以下R套件:
+1. 下載腳本至本機後，請使用R-Studio打開檔案並先安裝以下R套件:
 	
-	![](./img/packages_install.png)
+
+![](./img/packages_install.png)
 	
-	2. 請使用R之指令列鍵入安裝套件之函數，引號內輸入套件名稱，並執行安裝套件，Ex:
+2. 請使用R之指令列鍵入安裝套件之函數，引號內輸入套件名稱，並執行安裝套件，Ex:
 	
-	```R
-	install.pakages("tidyr")
-	```
+```R
+install.pakages("tidyr")
+```
+
+3. 待套件安裝好後，還需在執行前修改本機路徑及需要讀入之資料(黃色部分):
 	
-	3. 待套件安裝好後，還需在執行前修改本機路徑及需要讀入之資料(黃色部分):
+
+![](./img/setwd.png)
 	
-	![](./img/setwd.png)
+4. 最後按下腳本列右上角之Source鍵執行程式:
 	
-	4. 最後按下腳本列右上角之Source鍵執行程式:
-	
-	![](./img/run.png)
+
+![](./img/run.png)
 
 ------
 
-- ### 腳本分為四個R檔案，共輸出6個.xlsx檔案:
-
-**1. First_graph_(density_of_participants_by_age_ranges).R**
-執行後輸出2個.xlsx檔案:
+- ### 執行 SW_code_all.R 腳本共會輸出6個.xlsx檔案:
+	
 1. first_gragh.xlsx (年齡間距分布):
+	
 
 ![](./img/fg.png)
-
+	
 2. gender_over60.xlsx (性別人數):
+	
 
 ![](./img/genderover60.png)
-
-**2. Second_graph.R**
-執行後輸出second_graph.xlsx檔案 (各類型使用人次人數): 
+	
+3. second_graph.xlsx檔案 (各類型使用人次人數): 
+	
 
 ![](./img/sg.png)
 	
-**3. Third_graph.R**
-執行後輸2個.xlsx檔案
-1. Third_graph.xlsx (各區據點數及使用人次人數):
+4. Third_graph.xlsx (各區據點數及使用人次人數):
+	
 
 ![](./img/tg.png)
-
-2. Third_graph_org.xlsx (各據點使用人次人數):	
+	
+5. Third_graph_org.xlsx (各據點使用人次人數):	
+	
 
 ![](./img/tg2.png)
-
-**4. freqGraph.R**
-執行後輸出freqGraph.xlsx檔案 (各行政區據點一周平均刷卡人數): 
+	
+6. freqGraph.xlsx (各行政區據點一周平均刷卡人數): 
+	
 
 ![](./img/freq.png)
--- SW_code_all.R
+++ SW_code_all.R
@@ -131,3 +131,107 @@ export(admin.people.df, "first_gragh.xlsx")
 
 #write.csv(f.oversixty_normal_neat_older_adult_activity.df, "gender_over60.csv", row.names = FALSE)
 export(admin.people.df, "gender_over60.xlsx")
+
+
+#### Second graph (Total number of participants which is repeatable) #### 
+
+act.freq.table <- as.data.frame(table(normal_neat_older_adult_activity$子類型, normal_neat_older_adult_activity$主類型), stringsAsFactors=FALSE)
+act.freq.table <- act.freq.table[act.freq.table$Freq != 0,]
+
+colnames(act.freq.table) <- c("子類型", "主類型", "人次")
+act.freq.table[1,1:2] <- c("一般簽到", "一般簽到")
+row.names(act.freq.table) <- 1:nrow(act.freq.table)
+
+#### Second graph (Total number of participants which is not repeatable) #### 
+
+f.uni_normal_neat_older_adult_activity <- unique(normal_neat_older_adult_activity[,c(1,4,5,12,13,14,16)])
+
+norepo.act.freq.table <- as.data.frame(table(f.uni_normal_neat_older_adult_activity$子類型, f.uni_normal_neat_older_adult_activity$主類型), stringsAsFactors=FALSE)
+norepo.act.freq.table <- norepo.act.freq.table[norepo.act.freq.table$Freq != 0,]
+
+colnames(norepo.act.freq.table) <- c("子類型", "主類型", "人數")
+norepo.act.freq.table[1,1:2] <- c("一般簽到", "一般簽到")
+row.names(norepo.act.freq.table) <- 1:nrow(norepo.act.freq.table)
+
+#### merge and export csv file ####
+
+act.freq.table <- cbind(act.freq.table, 人數 = norepo.act.freq.table$人數)
+
+act.freq.table <- cbind(act.freq.table, 平均使用次數 = act.freq.table$人次/act.freq.table$人數)
+
+# write.csv(act.freq.table, "second_graph.csv", row.names = FALSE)
+export(admin.people.df, "second_graph.xlsx")
+
+#### Third graph (按照行政區)####
+
+admin.normal_neat_older_adult_activity <- as.data.frame(table(normal_neat_older_adult_activity$行政區))
+
+map.f.uni_normal_neat_older_adult_activity <- unique(normal_neat_older_adult_activity[,c(1,4,5,9,10,16)])
+admin.map.f.uni_normal_neat_older_adult_activity <- unique(normal_neat_older_adult_activity[,c(9,10)])
+
+admin.map.f.uni_normal_neat_older_adult_activity.df <- as.data.frame(table(admin.map.f.uni_normal_neat_older_adult_activity$行政區), stringsAsFactors=FALSE)
+colnames(admin.map.f.uni_normal_neat_older_adult_activity.df) <- c("行政區", "據點數")
+
+admin.uni.normal_neat_older_adult_activity <- as.data.frame(table(map.f.uni_normal_neat_older_adult_activity$行政區))
+
+admin.normal_neat_older_adult_activity <- cbind(admin.normal_neat_older_adult_activity,
+                                                人數 = admin.uni.normal_neat_older_adult_activity$Freq)
+
+colnames(admin.normal_neat_older_adult_activity) <- c("行政區", "人次", "人數")
+
+#### Third graph (按照據點流水號) ####
+
+org_list <- read.csv("108年度據點列表.csv")
+
+admin.normal_neat_older_adult_activity.by_id <- as.data.frame(table(normal_neat_older_adult_activity$據點流水號))
+
+uni.admin.normal_neat_older_adult_activity.by_id <- as.data.frame(table(map.f.uni_normal_neat_older_adult_activity$據點流水號))
+
+admin.normal_neat_older_adult_activity.by_id <- cbind(admin.normal_neat_older_adult_activity.by_id,
+                                                      人數 = uni.admin.normal_neat_older_adult_activity.by_id$Freq,
+                                                      平均使用次數 = admin.normal_neat_older_adult_activity.by_id$Freq/uni.admin.normal_neat_older_adult_activity.by_id$Freq,
+                                                      據點名稱 = org_list[org_list$據點流水號 %in% admin.normal_neat_older_adult_activity.by_id$Var1,]$據點名稱)
+
+colnames(admin.normal_neat_older_adult_activity.by_id)[1:2] <- c("據點流水號", "人次")
+
+admin.normal_neat_older_adult_activity <- cbind(admin.normal_neat_older_adult_activity,
+                                                "據點數" = admin.map.f.uni_normal_neat_older_adult_activity.df$據點數)
+
+# export csv and and xlsx
+# write.csv(admin.normal_neat_older_adult_activity.by_id, "Third_graph_org.csv", row.names = FALSE)
+export(admin.people.df, "Third_graph_org.xlsx")
+
+# write.csv(admin.normal_neat_older_adult_activity, "Third_graph.csv", row.names = FALSE)
+export(admin.people.df, "Third_graph.xlsx")
+
+
+freq.f.uni_normal_neat_older_adult_activity <- normal_neat_older_adult_activity[,c(1,2,4,5,9,10,16)]
+
+date.freq.f.uni_normal_neat_older_adult_activity <- separate(freq.f.uni_normal_neat_older_adult_activity, "簽到時間", into = c("日期", "時間"), sep = " ")
+date.freq.f.uni_normal_neat_older_adult_activity <- separate(date.freq.f.uni_normal_neat_older_adult_activity, "日期", into = c("年", "月", "日"), sep = "/")
+
+uni.date.freq.f.uni_normal_neat_older_adult_activity <- unique(date.freq.f.uni_normal_neat_older_adult_activity[,c(1,4,6,7,8,9)])
+
+norep.uni.date.freq.f.uni_normal_neat_older_adult_activity <- unique(date.freq.f.uni_normal_neat_older_adult_activity[,c(1,4,6,7),])
+
+all.hist.uni.date.freq.f.uni_normal_neat_older_adult_activity <- hist(table(norep.uni.date.freq.f.uni_normal_neat_older_adult_activity$長者流水號)/4, breaks = 6)
+
+all.hist.uni.date.freq.f.uni_normal_neat_older_adult_activity$counts
+
+admin.people.df <- data.frame()
+for(i in levels(uni.date.freq.f.uni_normal_neat_older_adult_activity$行政區)){
+  
+  uni.date.freq.f.uni_normal_neat_older_adult_activity.df <- as.data.frame(table(uni.date.freq.f.uni_normal_neat_older_adult_activity[uni.date.freq.f.uni_normal_neat_older_adult_activity$行政區 == i,]$長者流水號))
+  
+  uni.date.freq.f.uni_normal_neat_older_adult_activity.df.hist <- hist(uni.date.freq.f.uni_normal_neat_older_adult_activity.df$Freq/4, breaks = 6)
+  
+  temp.admin <- data.frame(一周平均刷卡天數 = 1:length(uni.date.freq.f.uni_normal_neat_older_adult_activity.df.hist$counts),
+                                   人數 = uni.date.freq.f.uni_normal_neat_older_adult_activity.df.hist$counts, 
+                                   行政區 = rep(i, length(uni.date.freq.f.uni_normal_neat_older_adult_activity.df.hist$counts)))
+  
+  admin.people.df <- rbind(admin.people.df, temp.admin)
+}
+
+#### export files
+
+export(admin.people.df, "freqGraph.xlsx")
